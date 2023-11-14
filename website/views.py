7ac229cec3049783de75fdcf2ec930d067a31d66
DanyLC91 <daniela.campisi@hotmail.com>
2023-11-14 11:23:27 +0000

    Request to send an emal to user added to views.py

--- website/views.py
+++ website/views.py
@@ -13,4 +13,13 @@ def exhibition(request):
     # Render the exhibition.html template
     return render(request, 'exhibition.html')
 def contact (request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print('Hello from Hawtrhorne Fine Art Gallery',
+              'We have recieve your message, and one of our agents will contact you son',
+              'This is a generated email.Please do not reply',
+              'contact@hawthorne.com')    
+        return HttpResponse('Form submitted successfully!')
     return render(request, 'contact.html')
